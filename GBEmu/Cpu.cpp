8ea1deb06a8d25bbec9269c5238343a66aede04b
Alriightyman <alriightyman@gmail.com>
2018-10-25 08:17:54 -0400

    Current Test
    Some changes

--- GBEmu/Cpu.cpp
+++ GBEmu/Cpu.cpp
@@ -1,200 +1,91 @@
 #include "Cpu.h"
 
-Cpu::Cpu() 
-	: m_PC(0x100),m_B(0),m_C(0x13),m_D(0),
-	m_E(0xD8),m_H(0x1),m_L(0x4D),m_SP(0xFFFE),
-	m_A(0x0),m_cycles(0)
-{
 
+
+Cpu::Cpu() 
+	: m_cycles(0)
+{
+	registers.A = 0x0;
+	registers.B = 0;
+	registers.C = 0x13;
+	registers.D = 0;
+	registers.E = 0xD8; 
+	registers.H = 0x1; 
+	registers.L = 0x4D;
+	registers.SP = 0xFFFE;
+	registers.PC = 0x100;
 }
 
 bool Cpu::Initialize()
 {
 
+
 	return true;
 }
 
+
 void Cpu::Run(Memory& mem)
 {
-	byte* opcode = &mem[m_PC];
+	byte* opcode = &mem[registers.PC];
+	
+	opcode_func method = opcodes[*opcode];
+	uint cycles = (this->*method)(mem);
 
-	switch (*opcode)
-	{
-	case 0x00: // nop - - - -
-		{
-			m_cycles += 4;
-		}
-		break;
-	case 0x01: // ld bc,d16 - - - -
-		{
-			m_B = opcode[1];
-			m_C = opcode[2];
-			m_PC += 2;
-			m_cycles += 12;
-		}
-		break;
-	case 0x02: // ld (bc),a - - - -
-		{
-			mem[((m_B << 8) | m_C)] = m_A;	
-			m_cycles +=  8; 
-		}
-		break;
-	case 0x03: // inc BC - - - -
-		{
-			short answer = (m_B << 8) | m_C;
-			answer++;
-			m_B = (answer >> 8) & 0xF;
-			m_C = answer & 0xF;
-			m_cycles += 8;
-		}
-		break;
-	case 0x04: // inc b z 0 h -
-		{
-			short answer = m_B + 1;
-			short hc = (m_B & 0xFF) + 1;
-			m_F.z = ((answer & 0xFF) == 0);
-			m_F.n = 0;
-			m_F.h = ((hc & 0xF) == 0 && m_B != 0xFF);
-			m_B = answer & 0xFF;
-			m_cycles += 4;
-		}
-		break;
-	case 0x05: // dec b z 1 h -
-		{
-			short answer = m_B - 1;
-			m_F.z = ((answer & 0xFF) == 0);
-			m_F.n = 1;
-			m_F.h = ((answer & 0xF) == 0xF && m_B != 0);
-			m_B = answer & 0xFF;
-			m_cycles += 4;
-		}
-		break;
-	case 0x06: // LD B,d8 - - - 
-		{
-			m_B = opcode[1];
-			m_PC++;
-			m_cycles += 8; 
-		}
-		break;
-	case 0x07: // RLCA 0 0 0 c
-		{
-			short answer = m_A << 1;
-			m_F.z = ((answer & 0xFF) == 0);
-			m_F.n = 0;
-			m_F.h = 0;
-			m_F.c = (answer > 0xFF);
-			m_A = answer & 0xFF;
-			m_cycles += 4;
-		}
-		break;
-	case 0x08: // LD (a16),sp - - - -
-		{
-			uint offset = opcode[1] << 8 | opcode[2];
-			mem[offset] = (m_SP >> 8 ) & 0xFF;
-			mem[offset + 1] = m_SP & 0xFF;
-			m_PC += 2;
-			m_cycles += 20;
-		}
-		break;
-	case 0x09: // add HL, BC - 0 h c
-		{
-			uint answer = ((m_H << 8) | m_L) + ((m_B << 8) | m_C);
-			ushort hc = answer & 0xFFFF;
-			m_F.n = 0;
-			m_F.c = (answer > 0xFFFF);
-			m_F.h = (answer );
-			m_cycles += 4;
-		}
-		break;
-
-	}
-
-	// increment opcode by one
-	m_PC++;
+	m_cycles += cycles;	
 }
 
-#ifdef FNCPTRS
-// ==========================================
-// function implementations
-// ==========================================
-// 1 byte, 4 cycles						// 00
-uint Cpu::NOP(Memory& mem)				
+#pragma region Opcode Functions
+
+#pragma endregion
+
+#pragma region Helper functions
+ // 16-bit Load command helper
+int Cpu::Load16BitImmediate(Memory mem, Registers* regs)
 {
-	m_PC++; 
+	ushort nn = mem[++regs->PC] << 8 | mem[++regs->PC];
+	regs->PC++;
+
+	return nn;
+}
+
+
+#pragma endregion
+
+uint Cpu::NOP(Memory & mem)
+{
+	this->registers.PC++;
 	return 4;
 }
-// 3 bytes, 12 cycles					// 01
-uint Cpu::LD_BCd16(Memory& mem)			
+
+uint Cpu::LD_BC_nn(Memory & mem)
 {
-	
-	m_PC++;
-	m_B = mem[m_PC] & 0xFF;
-	m_PC++;
-	m_C = mem[m_PC + 1] & 0xFF;
-	m_PC++;
+	registers.BC = Load16BitImmediate(mem, &this->registers);
 	return 12;
 }
-// 1 byte, 8 cycles						// 02
-uint Cpu::LD_addrBC_A(Memory& mem)		
-{
-	mem[(m_B << 8) | m_C] = m_A;
-	m_PC++;
-	return 8;
-}
-// 1 byte, 8 cycles						// 03
-uint Cpu::INC_BC(Memory& mem)			
-{
-	ushort d16 = ( m_B << 8 ) | m_C;
-	d16++;
-	m_B = (d16 >> 8) & 0xFF;
-	m_C = d16 & 0xFF;
-	m_PC++;
-	return 8;
-}
-// 1 byte, 4 cycles, Z 0 H -			// 04
-uint Cpu::INC_B(Memory& mem)			
-{
-	byte carry = m_B & 0xF;
-	m_B++;
-	if (m_B == 0)
-		SetFlagBit(ZFlag);
-	if (carry == 0xF)
-		SetFlagBit(HFlag);
 
-	ClearFlagBit(NFlag);
+uint Cpu::LD_addrBC_A(Memory & mem)
+{
+	registers.BC = registers.A;
+	registers.PC++;
+
+	return 8;
+}
+
+uint Cpu::INC_BC(Memory & mem)
+{
+	registers.BC++;
+	return 8;
+}
+
+uint Cpu::INC_B(Memory & mem)
+{
+	registers.B++;
+	registers.PC++;
+	byte z = (registers.B == 0);
+	byte n = 0; 
+	byte h = 0; // todo
+	byte c = 0; // todo
+
 
 	return 4;
 }
-// 1 byte, 4 cycles, Z 1 H -			// 05
-uint Cpu::DEC_B(Memory& mem)			
-{
-	byte carry = m_B & 0xF;
-	m_B--;
-	// if B is zero
-	if (m_B == 0)
-		SetFlagBit(ZFlag);
-	// if no borrow from bit 4
-	if (carry != 0)
-		SetFlagBit(HFlag);
-
-	SetFlagBit(NFlag);
-
-	return 4;
-}
-// 2 bytes, 8 cycles					// 06
-uint Cpu::LD_Bd8(Memory& mem)			
-{
-/*	
-	m_PC++;
-	m_B = mem[m_PC++];
-	return 8;
-*/
-	ld_nn_n(m_B);
-}
-// 1 byte, 4 cycles, 0 0 0 C			// 07
-uint Cpu::RLCA(Memory& mem)
-{
-	m_PC++;
-	return 4;
-}
-#endif
--- include/Cpu.h
+++ include/Cpu.h
@@ -1,9 +1,52 @@
 #pragma once
 #include "Memory.h"
+
 // emulates the gameboy cpu.  Similar to a Z80 and 8080 processor
+// See http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf for details
+
 /*				GB CPU Speed	NOP Instruction
 Machine Cycles		1.05MHz			1 cycle
 Clock Cycles		4.19MHz			4 cycles
+_____________________________________________________________________
+
+  Registers
+ -----------
+|15..8 7...0|
+|-----------|
+|  A  |  F  |
+|-----------|
+|  B  |  C  |
+|-----------|
+|  D  |  E  |
+|-----------|
+|  H  |  L  |
+|-----------|
+|     SP    |
+|-----------|
+|     PC    |
+ -----------
+ 
+ Flag Registers
+/---------------\
+|7|6|5|4|3|2|1|0|
+|---------------|
+|Z|N|H|C|0|0|0|0|
+\---------------/
+Zero Flag (Z):
+	This bit is set when the result of a math operation
+	is zero or two values match when using the CP
+	instruction.
+Subtract Flag (N):
+	This bit is set if a subtraction was performed in the
+	last math instruction.
+Half Carry Flag (H):
+	This bit is set if a carry occurred from the lower
+	nibble in the last math operation.
+Carry Flag (C):
+	This bit is set if a carry occurred from the last
+	math operation or if register A is the smaller value
+	when executing the CP instruction.
+
 */
 //// forward declaration
 //class Memory;
@@ -21,7 +64,7 @@ typedef unsigned short ushort;
 class Cpu
 {
 private:
-	struct FReg 
+/*	struct Flags 
 	{
 		byte z:1;
 		byte n:1;
@@ -29,21 +72,71 @@ private:
 		byte c:1;
 		byte unused:4;
 	};
-
+*/
+	typedef uint(Cpu::*opcode_func)(Memory&);
 private:
+
 #pragma region Member Variables
-	byte m_A;
-	byte m_B,m_C;
-	byte m_D,m_E;
-	byte m_H,m_L;
-	ushort m_SP; 
-	ushort m_PC;
-	FReg m_F;
+
+	struct Registers
+	{
+		union
+		{
+			struct
+			{
+				byte F;
+				byte A;
+			};
+			ushort AF;
+		};
+
+		union
+		{
+			struct
+			{
+				byte C;
+				byte B;
+			};
+			ushort BC;
+		};
+
+		union
+		{
+			struct
+			{
+				byte E;
+				byte D;
+			};
+			ushort DE;
+		};
+
+
+		union
+		{
+			struct
+			{
+				byte L;
+				byte H;
+			};
+			ushort HL;
+		};
+
+
+		ushort SP;
+		ushort PC;
+	};
 	uint m_cycles;
+
+	const opcode_func opcodes[255] =
+	{
+		&Cpu::NOP, &Cpu::LD_BC_nn, &Cpu::LD_addrBC_A, &Cpu::INC_BC, &Cpu::INC_B,&Cpu::DEC_B, &Cpu::LD_Bn, &Cpu::RLCA, &Cpu::LD_addrNN_SP, &Cpu::ADD_HLBC,
+		&Cpu::LD_AaddrBC, &Cpu::DEC_BC,
+		
+	};
 
 #pragma endregion
 
-
+	Registers registers;
 public:
 	Cpu();
 	bool Initialize();
@@ -52,74 +145,158 @@ public:
 #pragma region Accessors/Mutators
 	// Accessor/Mutators
 	// Regsiter A
-	byte GetRegA() const {return m_A;}
-	void SetRegA(byte value) { m_A = value; }
+	byte GetRegA() const {return registers.A;}
+	void SetRegA(byte value) { registers.A = value; }
 	// Register F, Flag Register
-	void ClearFlags() { m_F.c = m_F.h = m_F.n = m_F.z = m_F.unused = 0; }
-	bool GetZFlag() { return m_F.z; }
-	bool GetNFlag() { return m_F.n; }
-	bool GetHFlag() { return m_F.h; }
-	bool GetCFlag() { return m_F.c; }
-	void SetZFlag(bool bit) { m_F.z = bit; }
-	void SetNFlag(bool bit) { m_F.n = bit; }
-	void SetHFlag(bool bit) { m_F.h = bit; }
-	void SetCFlag(bool bit) { m_F.c = bit; }
-	// Reg B
-	byte GetRegB() const {return m_B;}
-	void SetRegB(byte value) { m_B = value; }
+	void ClearFlags() { registers.F = 0; } // m_Flags.c = m_Flags.h = m_Flags.n = m_Flags.z = m_Flags.unused = 0;
+
+	bool GetZFlag() 
+	{
+		return (bool)(registers.F >> 8) & 0xFC;
+	}
+	//bool GetNFlag() { return m_Flags.n; }
+	//bool GetHFlag() { return m_Flags.h; }
+	//bool GetCFlag() { return m_Flags.c; }
+	//void SetZFlag(bool bit) { m_Flags.z = bit; }
+	//void SetNFlag(bool bit) { m_Flags.n = bit; }
+	//void SetHFlag(bool bit) { m_Flags.h = bit; }
+	//void SetCFlag(bool bit) { m_Flags.c = bit; }
+	//// Reg B
+	byte GetRegB() const {return registers.B;}
+	void SetRegB(byte value) { registers.B = value; }
 	// register C
-	byte GetRegC() const {return m_C;}
-	void SetRegC(byte value) { m_C = value; }
+	byte GetRegC() const {return registers.C;}
+	void SetRegC(byte value) { registers.C = value; }
 	// register BC
-	short GetRegBC() const {return (m_B << 8) | m_C;}
-	void SetRegBC(short value) { m_B = (value >> 8) & 0xFF; m_C = value & 0xFF; }
+	short GetRegBC() const {return (registers.B << 8) | registers.C;}
+	void SetRegBC(short value) { registers.B = (value >> 8) & 0xFF; registers.C = value & 0xFF; }
 	// Register D
-	byte GetRegD() const {return m_D;}
-	void SetRegD(byte value) { m_D = value; }
+	byte GetRegD() const {return registers.D;}
+	void SetRegD(byte value) { registers.D = value; }
 	// register E
-	byte GetRegE() const {return m_E;}
-	void SetRegE(byte value) { m_E = value; }
+	byte GetRegE() const {return registers.E;}
+	void SetRegE(byte value) { registers.E = value; }
 	// register DE
-	short GetRegDE() const {return (m_D << 8) | m_E;}
-	void SetRegDE(short value) { m_D = (value >> 8) & 0xFF; m_E = value & 0xFF; }
+	short GetRegDE() const {return (registers.D << 8) | registers.E;}
+	void SetRegDE(short value) { registers.D = (value >> 8) & 0xFF; registers.E = value & 0xFF; }
 	// Register H
-	byte GetRegH() const {return m_H;}
-	void SetRegH(byte value) { m_H = value; }
+	byte GetRegH() const {return registers.H;}
+	void SetRegH(byte value) { registers.H = value; }
 	// register L
-	byte GetRegL() const {return m_L;}
-	void SetRegL(byte value) { m_L = value; }
+	byte GetRegL() const {return registers.L;}
+	void SetRegL(byte value) { registers.L = value; }
 	// register HL
-	short GetRegHL() const {return (m_H << 8) | m_L;}
-	void SetRegHL(short value) { m_H = (value >> 8) & 0xFF; m_L = value & 0xFF; }
+	short GetRegHL() const {return (registers.H << 8) | registers.L;}
+	void SetRegHL(short value) { registers.H = (value >> 8) & 0xFF; registers.L = value & 0xFF; }
 	// Register SP
-	short GetStackPointer() const { return m_SP; }
-	void SetStackPointer(short value) { m_SP = value; }
+	short GetStackPointer() const { return registers.SP; }
+	void SetStackPointer(short value) { registers.SP = value; }
 	// Register PC
-	short GetProgramCounter()const { return m_PC; }
-	void SetProgramCounter(short value) { m_PC = value; }
+	short GetProgramCounter()const { return registers.PC; }
+	void SetProgramCounter(short value) { registers.PC = value; }
 #pragma endregion
 private:
-#ifdef FNCPTRS
+//#ifdef FNCPTRS
+
+	static int Load16BitImmediate(Memory mem, Registers* regs);
+/* 
+opcode 0xXX X bytes, X cycles
+Descritpion: 
+args: 
+*/
 #pragma region Commands
 // all command return the number of cycles executed
-// 1 byte, 4 cycles					// 00
-uint NOP(Memory& mem);
-// 3 bytes, 12 cycles				// 01
-uint LD_BCd16(Memory& mem);	
-// 1 byte, 8 cycles					// 02
-uint LD_addrBC_A(Memory& mem);
-// 1 byte, 8 cycles					// 03
-uint INC_BC(Memory& mem);
-// 1 byte, 4 cycles, Z 0 H -		// 04
-uint INC_B(Memory& mem);
-// 1 byte, 4 cycles, Z 1 H -		// 05
-uint DEC_B(Memory& mem);
-// 2 bytes, 8 cycles				// 06
-uint LD_Bd8(Memory& mem);
-// 1 byte, 4 cycles, 0 0 0 C		// 07
-uint RLCA(Memory& mem);
+
+	// opcode 0x00 1 byte, 4 cycles
+	uint NOP(Memory& mem);
+
+	/* opcode 0x01 3 bytes, 12 cycles
+	 Descritpion: Put value nn into BC
+	 args: nn = 16 bit immediate value */
+	uint LD_BC_nn(Memory& mem);
+
+	/* opcode 0x02 1 bytes, 8 cycles
+	 Descritpion: Put A into (BC) */
+	uint LD_addrBC_A(Memory& mem);
+
+	/* opcode 0x03 1 bytes, 8 cycles
+	 Descritpion: Increment register BC
+	 Flags: NONE */
+	uint INC_BC(Memory& mem);
+
+	/* opcode 0x04 1 bytes, 4 cycles
+	 Descritpion: Increment register B
+	 Flags:
+		Z - Set if result is zero.
+		N - Reset.
+		H - Set if carry from bit 3.
+		C - Not affected */
+	uint INC_B(Memory& mem);
+
+	/*
+	opcode 0x05 1 bytes, 4 cycles
+	Descritpion: Decrement register B
+	Flags:
+		Z - Set if reselt is zero.
+		N - Set.
+		H - Set if no borrow from bit 4.
+		C - Not affected.
+	*/
+	uint DEC_B(Memory& mem);
+
+	/*
+	opcode 0x06 2 bytes, 8 cycles
+	Descritpion: Put value n into register B
+	args: n = 8 bit immediate value
+	*/
+	uint LD_Bn(Memory& mem);
+
+	/*
+	opcode 0x07 1 bytes, 4 cycles
+	Descritpion: Rotate A left. Old bit 7 to Carry flag.
+	Flags:
+		Z - Set if result is zero.
+		N - Reset.
+		H - Reset.
+		C - Contains old bit 7 data.
+	*/
+	uint RLCA(Memory& mem);
+
+	/*
+	opcode 0x08 3 bytes, 20 cycles
+	Descritpion: Put Stack Pointer (SP) at address nn
+	args: nn = two byte immediate address.
+	*/
+	uint LD_addrNN_SP(Memory& mem);
+
+	/*
+	opcode 0x09 1 bytes, 8 cycles
+	Descritpion: Add BC to HL.
+	Flags:
+		Z - Not affected.
+		N - Reset.
+		H - Set if carry from bit 11.
+		C - Set if carry from bit 15.
+	*/
+	uint ADD_HLBC(Memory& mem);
+
+	/*
+	opcode 0x0A 1 bytes, 8 cycles
+	Descritpion: put value of (BC) in A
+	args: (BC) is address 
+	*/
+	uint LD_AaddrBC(Memory& mem);
+
+	/*
+	opcode 0x0B 1 bytes, 8 cycles
+	Descritpion: Decrement BC
+	Flags: NONE
+	*/
+	uint DEC_BC(Memory& mem);
+
 
 #pragma endregion
-#endif
+
+//#endif
 };
 
